00566e3592ff28d1e6f79055b3e618c85c3cdcaa
pdruart <pdruart@student.codam.nl>
2022-03-17 17:09:34 +0100

    changed ceiling/floor colors to a separate hook for pixel putting optimization

--- src/game/main.c
+++ src/game/main.c
@@ -6,7 +6,7 @@
 /*   By: rdrazsky <rdrazsky@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/17 15:34:12 by rdrazsky      #+#    #+#                 */
-/*   Updated: 2022/03/17 16:35:25 by rdrazsky      ########   odam.nl         */
+/*   Updated: 2022/03/17 17:05:55 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 static void	static_main_hook(void *param)
 {
 	t_data *const	data = param;
-	UINT			i;
 	t_iv			cord;
 
 	cord.x = 0;
@@ -30,20 +29,7 @@ static void	static_main_hook(void *param)
 		cord.x++;
 	}
 	data->bitmode = !data->bitmode;
-	i = 0;
-	// printf("fps:%f,depth:%i\n", 1.0 / data->mlx->delta_time, data->ray_depth);
 	data->game_time += data->mlx->delta_time;
-	// while (i < data->screen->width * data->screen->height / 2
-	// 	+ data->player_ud_angle * data->screen->width)
-	// {
-	// 	mlx_putpixel(data->screen, i, 0, data->roof);
-	// 	i++;
-	// }
-	// while (i < data->screen->width * data->screen->height)
-	// {
-	// 	mlx_putpixel(data->screen, i, 0, data->floor);
-	// 	i++;
-	// }
 	if (data->ray_depth > 2 && mlx_is_key_down(data->mlx, MLX_KEY_KP_SUBTRACT))
 		data->ray_depth--;
 	if (data->ray_depth < 200 && mlx_is_key_down(data->mlx, MLX_KEY_KP_ADD))
@@ -51,6 +37,28 @@ static void	static_main_hook(void *param)
 	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(data->mlx);
 	// ft_print(1, "\x1b[A{ FPS: %i }\n", (int)(1 / data->mlx->delta_time));
+}
+
+static void	static_background_hook(void *param)
+{
+	t_data *const	data = param;
+	float			height;
+	UINT			i;
+
+	i = 0;
+	while (i < WIDTH * HEIGHT)
+	{
+		if (data->rays[i % WIDTH].wall_type)
+			height = fminf(fabsf(HEIGHT / data->rays[i % WIDTH].eye_len),
+					100000);
+		else
+			height = 0;
+		if (i < WIDTH * (HEIGHT / 2 - height / 2 + data->player_ud_angle))
+			mlx_putpixel(data->screen, i, 0, data->roof);
+		else if (i > WIDTH * (HEIGHT / 2 + height / 2 + data->player_ud_angle))
+			mlx_putpixel(data->screen, i, 0, data->floor);
+		i++;
+	}
 }
 
 int32_t	main(int argc, char **argv)
@@ -68,8 +76,9 @@ int32_t	main(int argc, char **argv)
 	init_game(&data);
 	mlx_loop_hook(data.mlx, &static_main_hook, &data);
 	mlx_loop_hook(data.mlx, &ray_cast_hook, &data);
-	mlx_loop_hook(data.mlx, (void (*)(void *))map_hook, &data);
+	mlx_loop_hook(data.mlx, &static_background_hook, &data);
 	mlx_loop_hook(data.mlx, (void (*)(void *))ceiling_hook, &data);
+	mlx_loop_hook(data.mlx, (void (*)(void *))map_hook, &data);
 	mlx_loop_hook(data.mlx, (void (*)(void *))sprite_hook, &data);
 	mlx_loop_hook(data.mlx, &player_movement_hook, &data);
 	mlx_loop_hook(data.mlx, (void (*)(void *))minimap_hook, &data);
